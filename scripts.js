a50b2e977663782f3ccf71471660c711c856f537
Vincent Huang <vh222@cornell.edu>
2023-06-22 04:33:06 -0400

    Messing around with limiting lines

--- scripts.js
+++ scripts.js
@@ -1,36 +1,53 @@
 $(document).ready(function() {
     const buttons = document.querySelectorAll(".key");
-    console.log(buttons);
     const divElements = document.querySelectorAll(".tile");
-    console.log(divElements);
     const delete_btn = document.querySelector(".delete");
-    console.log(delete_btn);
     const enter_btn = document.querySelector(".enter");
-    console.log(enter_btn);
 
     let chars = [];
 
-    var i = 0;
+    var tileNum = 0;
+    var rowNum = 0;
     buttons.forEach(key => {
         key.addEventListener('click', () => { //need to prevent user from being able to type onto next row unless enter is pressed
-            divElements[i].textContent = key.innerText
-            chars.push(key.innerText)
-            i++
+            if ((tileNum + (rowNum * 5)) < 30) {
+                if (tileNum<=4) {
+                    if (tileNum == 4 && divElements[tileNum + (rowNum * 5)].textContent.trim().length > 0) {
+                        return;
+                    }
+                    divElements[tileNum + (rowNum * 5)].textContent = key.innerText;
+                    chars.push(key.innerText);
+                    if (tileNum < 4) {
+                        tileNum++;
+                    } 
+                }
+            }
         })
     })
 
     delete_btn.addEventListener('click', () => {
-        if (i!=0) {
-            chars.pop()
-            i--
-            divElements[i].textContent = ''
+        console.log("I'm deleting at " + (tileNum + (rowNum * 5)));
+        if (tileNum == 0) {
+            chars.pop();
+            divElements[tileNum + (rowNum * 5)].textContent = '';
+        }
+        if (tileNum > 0) {
+            chars.pop();
+            divElements[tileNum + (rowNum * 5)].textContent = '';
+            tileNum--;
         }
     })
 
     enter_btn.addEventListener('click', () => { //need to add check to make sure that the answer is a word and is of length 5
-        let answer = chars.join('')
-        if (answer == "HELLO") { //need to remember that the value of each key is CAPITALIZED
-            console.log("this is run if enter button works")
+        if (tileNum == 4) {
+            let answer = chars.join('');
+            if (answer == "HELLO") { //need to remember that the value of each key is CAPITALIZED
+                console.log("this is run if enter button works");
+            }
+            rowNum ++;
+            tileNum = 0;
+            chars = [];
+            console.log("You pressed: enter and incremented rowNum to " + (rowNum));
         }
     })
 });
